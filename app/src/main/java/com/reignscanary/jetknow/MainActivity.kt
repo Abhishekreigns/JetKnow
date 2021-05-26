5dc1836ae53c0ed94db72cf9e110b6c2f301d35d
Mr.Reignz <abhishek7reigns@gmail.com>
2021-05-26 20:14:22 +0530

    Added Visible Map and adjusted a bit

--- app/src/main/java/com/reignscanary/jetknow/MainActivity.kt
+++ app/src/main/java/com/reignscanary/jetknow/MainActivity.kt
@@ -3,23 +3,31 @@ package com.reignscanary.jetknow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.gms.maps.model.LatLng
 import com.reignscanary.jetknow.composables.CustomMapView
 import com.reignscanary.jetknow.composables.HostOfComposables
 import com.reignscanary.jetknow.composables.MainScreenViewModel
 import com.reignscanary.jetknow.composables.SearchText
+import com.reignscanary.jetknow.ui.theme.JetKnowTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            JetKnowTheme() {
+                HostOfComposables(savedInstanceState = savedInstanceState)
+            }
 
-           HostOfComposables()
+            }
+          
 
 
             }
-        }
+
     }
 
 
--- app/src/main/java/com/reignscanary/jetknow/composables/HomeScreen.kt
+++ app/src/main/java/com/reignscanary/jetknow/composables/HomeScreen.kt
@@ -1,43 +1,54 @@
 package com.reignscanary.jetknow.composables
 
-import android.content.Context
+
+import android.os.Bundle
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.maps.model.MarkerOptions
-import com.reignscanary.jetknow.ui.theme.Shapes
+
 
 @Composable
-fun HostOfComposables(mainScreenViewModel: MainScreenViewModel = viewModel())
-{
+fun HostOfComposables(mainScreenViewModel: MainScreenViewModel = viewModel(),savedInstanceState : Bundle?)
+{ val scrollState = rememberScrollState()
+
     val searchText :String by mainScreenViewModel.searchText.observeAsState("" )
-val roundedBox = RoundedCornerShape(10.dp)
-Column(modifier = Modifier.padding(8.dp)
+val roundedBox = RoundedCornerShape(16.dp)
+Column(modifier = Modifier
+    .padding(8.dp)
+    .fillMaxSize(1f)
         ) {
 
-    SearchText(searchText = searchText) { mainScreenViewModel.onSearchTextChange(it) }
+    SearchText(searchText = searchText,modifier = Modifier.fillMaxWidth(0.9f)) { mainScreenViewModel.onSearchTextChange(it) }
+ Spacer(modifier = Modifier.padding(10.dp))
+    CustomMapView(DEFAULT_LOCATION = LatLng(35.7676325, 51.3192201),
+        savedInstanceState = savedInstanceState,modifier = Modifier
+            .padding(2.dp)
+            .fillMaxHeight(0.9f)
+            .clip(roundedBox))
+    Spacer(modifier = Modifier.padding(2.dp))
+    FloatingActionButton(onClick = { /*TODO*/ }) {
+        
 
-    CustomMapView(DEFAULT_LOCATION = LatLng(35.7676325, 51.3192201),modifier = Modifier
-        .clip(roundedBox)
-        .background(Color.Gray)
-
-        )
+    }
 
 }
 
@@ -47,40 +58,41 @@ Column(modifier = Modifier.padding(8.dp)
 
 
 @Composable
-fun CustomMapView(DEFAULT_LOCATION: LatLng,modifier: Modifier = Modifier)
+fun CustomMapView(DEFAULT_LOCATION: LatLng, savedInstanceState : Bundle?, modifier: Modifier = Modifier)
 {
-
+val mapView = MapView(LocalContext.current)
  AndroidView(
-        modifier = modifier.fillMaxSize(),
-        factory = { context: Context ->
-            MapView(context).apply {
+     modifier = modifier.fillMaxSize(),
+     factory = { mapView.apply {
 
-                this.getMapAsync{
+         this.onCreate(savedInstanceState)
+         this.getMapAsync{
 
-                    it.addMarker(
-                        MarkerOptions()
-                        .position(DEFAULT_LOCATION)
-                        .title("Marker")
-                    )
-                }
+             it.addMarker(
+                 MarkerOptions()
+                     .position(DEFAULT_LOCATION)
+                     .title("Marker")
 
-            }
-        },
+             )
+         }
 
+     }
 
-        )
+               },
+ )
+
 
 }
-
 @Composable
 fun SearchText(modifier: Modifier = Modifier,searchText : String,onSearchTextChange: (String) -> Unit)
 {
     OutlinedTextField(value =  searchText,
         onValueChange = onSearchTextChange,
         modifier
-            .fillMaxHeight(0.08f)
+            .fillMaxHeight(0.1f)
             .fillMaxWidth()
-            .padding(4.dp)
+            .padding(4.dp),
+
 
 
     )
